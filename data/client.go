e024c19028c14f123a2a57c0ec6e51e063ed6e40
robertosnap <Jakkolako123>
2017-10-26 21:44:40 +0200

    Refactor main

--- data/client.go
+++ data/client.go
@@ -5,9 +5,13 @@ import (
 	"io/ioutil"
 )
 
+//MAINNET
 //var NemNodeUrl = "http://alice5.nem.ninja:7890"
-//var NemNodeUrl = "http://192.3.61.243:7890"
-var NemNodeUrl = "http://10.0.0.207:7890"
+//var NemNodeUrl = "http://10.0.0.207:7890"
+
+//TESTNET
+// var NemNodeUrl = "http://192.3.61.243:7890"
+var NemNodeUrl = "http://127.0.0.1:7890"
 
 func Get(endpoint string) []byte {
 	resp, err := http.Get(getNode() + endpoint)
--- main.go
+++ main.go
@@ -5,239 +5,171 @@ import (
 	"os"
 	"strconv"
 	"github.com/robertosnap/nemcrawler/data"
-	"github.com/robertosnap/nemcrawler/models"
-	json2 "encoding/json"
+	"encoding/json"
 	"io/ioutil"
 )
 
+var Namespace string = "banco"
+var Mosaic string = "coin"
+var depth = 3
+
 func main() {
 	//runCrawler("dim", "coin")
-	//runCrawler("dim", "coin")
-	runCrawler("nemventory.product", "beginners_fishing_rod")
+	//runCrawler("banco", "coin")
+	//runCrawler("nemventory.product", "beginners_fishing_rod")
 	//runCrawler("breeze", "breeze-token")
+	//runCrawler("gold", "gold")
+	run()
 
 }
-type mosaicCrawler struct {
-	Namespace     string
-	Mosaic        string
-	Creator       string
-	Address       string
-	InitialSupply int
+
+type network struct {
+	nodes nodes
+	edges edges
 }
-
-type Edges []edge
-
-type edge struct{
+type nodes []node
+type node struct {
+	Address string `json:"id"`
+	Wave int `json:"group"`
+	Label string `json:"label"`
+	Amount int `json:"value"`
+}
+type edges []edge
+type edge struct {
 	From string `json:"from"`
 	To string `json:"to"`
+	Transfer int `json:"value"`
+	Wave int `json:"group"`
 }
 
-type waveParams struct {
-	Address string
-	Quantity int
-	Count int
-}
+func run () {
+	var n = network{}
+	address, _ := getAddressSupply(Namespace, Mosaic)
 
-type AllWaves []waveTrans
+	waveCount := 1
 
-type allData struct {
-	AllWaves `json:"nodes"`
-	Edges `json:"edges"`
-}
+	n.addNode(address, waveCount)
 
-type waveTrans struct {
-	Count int `json:"group"`
-	Sender string `json:"address"`
-	Amount int `json:"value"`
-	Receivers []transferTrans
-}
-
-type transferTrans struct {
-	Address string
-	Amount int
-}
-
-func runCrawler(namespace string, mosaic string) {
-	//Find out who is owner of the mosaic
-	m := mosaicCrawler{
-		Namespace: namespace,
-		Mosaic:    mosaic,
+	waveCount++
+	for _,e := range n.edges{
+		n.addNode(e.To, waveCount)
 	}
-
-	//Get creator of coins and how much coins are printed.
-	m.getReady()
-
-	//Wave 1
-	w1 := initWave(m)
-	all := AllWaves{}
-	all = append(AllWaves{}, w1)
-
-	//Wave 2
-	w2 := startWaveing(w1,m, all)
-	all = append(all, w2...)
-
-	//Wave 3
-	for _,w := range all {
-		if w.Count == 2 {
-			all = append(all, startWaveing(w,m,all)...)
+	for waveCount <= depth {
+		waveCount++
+		for _,e := range n.edges{
+			if(e.Wave == waveCount-1){
+				n.addNode(e.To, waveCount)
+			}
 		}
 	}
-
-	//Wave 4
-	for _,w := range all {
-		if w.Count == 3 {
-			all = append(all, startWaveing(w,m,all)...)
-		}
-	}
-
-	Edges := Edges{}
-	/*Create Edges*/
-	for _,w := range all {
-		for _,k := range w.Receivers {
-			Edges = append(Edges, edge{
-				From: w.Sender,
-				To: k.Address,
-			})
-
-		}
-	}
-
-	allData := allData{
-		AllWaves: all,
-		Edges: Edges,
-	}
-
-
 
 	/*Print to file*/
-	fmt.Printf("\n %+v \n", allData)
-
-	result, err := json2.Marshal(allData)
+	result, err := json.Marshal(n)
 	if err != nil {
 		fmt.Println("Problem when converting result to JSON", err)
 		os.Exit(1)
 	}
-	filename :=  "public/data/"+m.Namespace + "_" + m.Mosaic+".json"
+	filename :=  "public/data/"+ Namespace + "-" + Mosaic +".json"
 	err = ioutil.WriteFile(filename, result, 0666)
-
-	//Save the mosaic transactions
-
+	if err != nil {
+		fmt.Println("Problem creating JSON file", err)
+		os.Exit(1)
+	}
 }
 
+func (n *network)addNode(address string, wave int) {
+	var nodeExist bool
 
-
-
-func startWaveing(w waveTrans, m mosaicCrawler, allWaves AllWaves) AllWaves {
-	newWaveCount := w.Count + 1
-
-	newWaves := AllWaves{}
-
-	for _,r := range w.Receivers {
-		allTransactions := data.GetAccountTransfersOutgoingAll(r.Address)
-		newWaveParams := waveParams{
-			Address: r.Address,
-			Quantity: r.Amount,
-			Count: newWaveCount,
+	//Check that this account does is not allready added to nodes list.
+	for _,node := range n.nodes {
+		if node.Address == address{
+			nodeExist = true
 		}
-		//Check if sender has been used before
-		for _,z := range allWaves  {
-			if z.Sender == r.Address {
-				continue
-			}
-		}
-		for _,g := range newWaves  {
-			if g.Sender == r.Address {
-				continue
-			}
-		}
-
-		newWaves = append(newWaves, newWave(m, allTransactions,newWaveParams) )
 	}
 
-	return newWaves
+	//Get how much this address has of this mosaic
+	var amount int
+	amount = amountMosaic(address)
 
+	if ! nodeExist {
+		//Create the node
+		n.nodes = append(n.nodes,node{
+			Address: address,
+			Wave: wave,
+			Label: address,
+			Amount: amount,
+		})
+	}
+
+	//Add edges
+	n.addEdges(address, amount, wave)
 }
 
-func initWave(m mosaicCrawler) waveTrans{
-	allTransactions := data.GetAccountTransfersOutgoingAll(m.Address)
-
-	var waveCount = 1
-
-	params := waveParams{
-		Address: m.Address,
-		Quantity: m.InitialSupply,
-		Count: waveCount,
-	}
-	return newWave(m, allTransactions,params)
-}
-
-
-
-func newWave(m mosaicCrawler, a models.AccountTransfersOutgoing, params waveParams) waveTrans {
-	w := waveTrans{
-		Count: params.Count,
-		Sender: params.Address,
-	}
-
-	//get how much this account has
-	owner := data.GetAccountMosaicOwned(params.Address)
-	for _,o := range owner.Data {
-		if m.Namespace == o.MosaicID.NamespaceID && m.Mosaic == o.MosaicID.Name {
-			w.Amount = o.Quantity
-		}
-	}
-
-	countedAmount := 0
-	//Check for multisig and noremalize call to filter
-	for _, v := range a.Data {
+func (n *network)addEdges(address string, totalAmountOwn int, wave int){
+	var totalAmountSent = 0
+	fmt.Println("=====================")
+	fmt.Printf("Got %v on %v %v\n", totalAmountOwn, address, Namespace+"."+Mosaic)
+	at := data.GetAccountTransfersOutgoingAll(address)
+	for _, v := range at.Data {
 		if v.Multisig() {
 			if v.Transaction.OtherTrans.Type == 257 {
-				for _,mosaic := range v.Transaction.OtherTrans.Mosaics{
-					if mosaic.MosaicID.NamespaceID == m.Namespace && mosaic.MosaicID.Name == m.Mosaic {
-						w.Receivers = append(w.Receivers, transferTrans{
-							Address: v.Transaction.OtherTrans.Recipient,
-							Amount: mosaic.Quantity,
-						} )
-						countedAmount += mosaic.Quantity
+				for _, mosaic := range v.Transaction.OtherTrans.Mosaics {
+					if mosaic.MosaicID.NamespaceID == Namespace && mosaic.MosaicID.Name == Mosaic {
+						n.addEdge(address, v.Transaction.OtherTrans.Recipient, mosaic.Quantity, wave)
+						totalAmountSent += mosaic.Quantity
+						fmt.Printf("Sent: %v -> %v (Multisig)\n",mosaic.Quantity, v.Transaction.OtherTrans.Recipient )
 					}
 				}
 			}
-		}else{
+		} else {
 			//Not multisig
 			if v.Transaction.Type == 257 {
-				for _,mosaic := range v.Transaction.Mosaics{
-					if mosaic.MosaicID.NamespaceID == m.Namespace && mosaic.MosaicID.Name == m.Mosaic {
-						w.Receivers = append(w.Receivers, transferTrans{
-							Address: v.Transaction.Recipient,
-							Amount: mosaic.Quantity,
-						} )
-						countedAmount += mosaic.Quantity
+				for _, mosaic := range v.Transaction.Mosaics {
+					if mosaic.MosaicID.NamespaceID == Namespace && mosaic.MosaicID.Name == Mosaic {
+						n.addEdge(address, v.Transaction.Recipient,mosaic.Quantity, wave)
+						totalAmountSent += mosaic.Quantity
+						fmt.Printf("Sent: %v -> %v (regular)\n ",mosaic.Quantity, v.Transaction.Recipient )
 					}
 				}
 			}
 		}
 	}
-
-	//Check if all the supply is counted
-	if params.Quantity == w.Amount + countedAmount {
-		fmt.Printf("All supply counted. Total: %v \n",params.Quantity )
-	}else{
-		fmt.Printf("Have been sent: %v --- Account amount: %v \n", params.Quantity , w.Amount + countedAmount)
-		fmt.Printf("\n ***    Account: %v \n", w.Sender)
-	}
-
-	return w
+	fmt.Printf("Total Sent: %v| Everyhting sent: %v \n", totalAmountSent, totalAmountSent == totalAmountOwn)
+	fmt.Println("=====================")
+	fmt.Println("")
 }
 
-func (m *mosaicCrawler) getReady() {
 
-	NamespaceMosaicDefinitionPage := data.GetNamespaceMosaicDefinitionPage(m.Namespace)
+func (n *network)addEdge( addressFrom string, addressTo string, amount int, wave int) {
+	n.edges = append(n.edges,edge{
+		From: addressFrom,
+		To: addressTo,
+		Transfer: amount,
+		Wave: wave,
+	})
+}
 
-	//Get mosaic, creator and supply
-	for _, v := range NamespaceMosaicDefinitionPage.Data {
-		if m.Namespace == v.Mosaic.ID.NamespaceID && m.Mosaic == v.Mosaic.ID.Name {
-			println("Creator found, initiating mapping starting with account:", v.Mosaic.Creator)
-			m.Creator = v.Mosaic.Creator
+func amountMosaic(address string) (amount int) {
+	//get how much this account has of namespace and mosaic
+	owner := data.GetAccountMosaicOwned(address)
+	for _,o := range owner.Data {
+		if Namespace == o.MosaicID.NamespaceID && Mosaic == o.MosaicID.Name {
+			amount = o.Quantity
+		}
+	}
+	return amount
+}
+
+func getAddressSupply(namespace string, mosaic string) (address string, initialSupply int) {
+	//Get all mosaics for this account
+	mosaics := data.GetNamespaceMosaicDefinitionPage(namespace)
+
+	//Only look at the spesific mosaic for this case
+	for _, v := range mosaics.Data {
+		if namespace == v.Mosaic.ID.NamespaceID && mosaic == v.Mosaic.ID.Name {
+			//get the address from public key
+			address = data.GetAccountFromPublicKey(v.Mosaic.Creator).Account.Address
+			println("Address mosaic creator found. Starting to map ", address)
 
 			//Find the property with initial supply and set it.
 			for _, value := range v.Mosaic.Properties {
@@ -247,28 +179,23 @@ func (m *mosaicCrawler) getReady() {
 						fmt.Println("Could not convert initialSupply string to INT \n")
 						os.Exit(1)
 					}
-					m.InitialSupply = int
+					initialSupply = int
 				}
 			}
 		}
 	}
+	//Check that we get information needed.
+	if initialSupply == 0 {
+		fmt.Printf("Initial supply for mosaic: %v with namespace: %v is 0 or not found. \n", mosaic, namespace)
+		os.Exit(1)
+	}
+	if address == "" {
+		fmt.Printf("Address for mosaic: %v with namespace: %v is 0 or not found. \n", mosaic, namespace)
+		os.Exit(1)
+	}
 
-	//Get address for creator
-	m.Address = data.GetAccountFromPublicKey(m.Creator).Account.Address
-
-	//Test that we have required info.
-	if m.Creator == "" {
-		fmt.Printf("No crator of mosaic: %v with namespace: %v found. \n", m.Mosaic, m.Namespace)
-		os.Exit(1)
-	}
-	if m.InitialSupply == 0 {
-		fmt.Printf("Initial supply for mosaic: %v with namespace: %v is 0 or not found. \n", m.Mosaic, m.Namespace)
-		os.Exit(1)
-	}
-	if m.Address == "" {
-		fmt.Printf("Address for mosaic: %v with namespace: %v is 0 or not found. \n", m.Mosaic, m.Namespace)
-		os.Exit(1)
-	}
+	return address, initialSupply
 }
+
 
 
